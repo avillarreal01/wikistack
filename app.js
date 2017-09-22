66800e8d24dc42525b9a2f23f8124fa81fa0465e
Alejandro Villarreal <avillarreal01@gmail.com>
2017-09-22 16:15:22 -0400

    project init

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const morgan = require('morgan');
+const routes = require('./routes');
+const nunjucks = require('nunjucks');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+//Setup nunjucks
+var env = nunjucks.configure('views', {noCache: true});
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+
+const server = app.listen(4000);
+
+//middleware
+app.user(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
+
+app.use('/', routes);
---
+++ routes/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const router = express.Router();
+
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+
+router.get('/', (req, res) => res.render('index'));
