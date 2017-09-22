90813ff123034dbdbb9df5fbd80467d797a1a14b
Jerry <jerrymuzsik@icloud.com>
2017-09-22 17:15:14 -0400

    routes, etc.

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const wiki = require('./wiki.js');
+const user = require('./user.js');
 
 const router = express.Router();
 
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
-router.get('/', (req, res) => res.render('index'));
+router.get('/', (req, res) => {
+
+  res.render('index'));
+}
+
+router.get('/wiki/', (req,res) => {
+
+})
+
+router.post('/wiki/', (req,res) => {
+
+})
+
+
 
 module.exports = router;
