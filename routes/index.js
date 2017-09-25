c7cf4fd6a0d87868f2f26bd5cd0c7de0cb38c57b
Alejandro Villarreal <avillarreal01@gmail.com>
2017-09-25 10:00:36 -0400

    addpage

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const wiki = require('./wiki.js');
-const user = require('./user.js');
+const wikiRouter = require('./wiki.js');
+const userRouter= require('./user.js');
 
 const router = express.Router();
 
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
+router.use('/wiki', wikiRouter);
+router.use('/user', userRouter);
+
 router.get('/', (req, res) => {
 
   res.render('index');
 });
-
-router.get('/wiki/', (req,res) => {
-
-});
-
-router.post('/wiki/', (req,res) => {
-
-});
-
-
 
 module.exports = router;
--- routes/wiki.js
+++ routes/wiki.js
@@ -1,5 +1,16 @@
 const express = require('express');
-
 const router = express.Router();
+
+router.get('/', (req,res) => {
+  res.send('GET Wiki');
+});
+
+router.post('/', (req,res) => {
+  res.send('POST wiki');
+});
+
+router.get('/add', (req,res) => {
+  res.render('addpage');
+});
 
 module.exports = router;
